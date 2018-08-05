1d8f13668c1bdf2163fa01cd0cfaaaaa7f99c7b2
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-05 10:36:07 +0100

    connected both pages main screen and account creation

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -7,10 +7,15 @@ import java.util.ResourceBundle;
 /* Import javafx, java, mainPackage */
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable {
 	@FXML
@@ -31,7 +36,17 @@ public class LoginController implements Initializable {
 	}
 	
 	@FXML
-	protected void handleBtnCreate(ActionEvent event) {
+	protected void handleBtnCreate(ActionEvent event) throws IOException {
 		System.out.println("Create new account btn pressed");
+		
+		
+		Parent parent = FXMLLoader.load(getClass().getResource("/fxml/createAccountPage.fxml"));
+		Scene scene = new Scene(parent);
+		Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		appStage.setScene(scene);
+		appStage.setTitle("Create Account");
+		appStage.setWidth(944);
+		appStage.setHeight(600);
+		appStage.show();
 	}
 }
