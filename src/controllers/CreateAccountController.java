68951753afcac405b0191fb9f03b317a6de425d1
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-06 06:34:14 +0100

    added controllers for pages

---
+++ src/controllers/CreateAccountController.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/* Import javafx, java, mainPackage */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class CreateAccountController implements Initializable {
+	@FXML
+	private Text actiontarget;
+	@FXML
+	private PasswordField passwordField;
+	@FXML
+	private TextField userField;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+	@FXML
+	protected void handleBtnBack(ActionEvent event) throws IOException {
+		System.out.println("Back btn pressed");
+		
+		// Send us back to main screen
+		Parent parent = FXMLLoader.load(getClass().getResource("/fxml/mainPage.fxml"));
+		Scene scene = new Scene(parent);
+		Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		appStage.setScene(scene);
+		appStage.setTitle("Calories Tracker");
+		appStage.setWidth(944);
+		appStage.setHeight(600);
+		appStage.show();
+		
+	}
+	
+	@FXML
+	protected void handleBtnSubmitAccount(ActionEvent event) throws IOException {
+		System.out.println("Try to create an account now!");
+	}
+}
