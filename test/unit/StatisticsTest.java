5c11dd4c720ed10ad7481cbce3cefb8b41c6ee11
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-06 19:03:36 +0100

    re added tests

---
+++ test/unit/StatisticsTest.java
@@ -0,0 +1,14 @@
+package unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StatisticsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
