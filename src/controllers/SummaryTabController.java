4877480b767689e5cedc98a39acb5fdf657a1f04
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-06 19:01:18 +0100

    added summary tab, and a chart all linked with a controller/fxml page for easier editing

---
+++ src/controllers/SummaryTabController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+/* Import java, javafx, mainPackage */
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SummaryTabController implements Initializable {
+	
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+}
