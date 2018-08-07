e62759898ee52ffa3752d68c5d6e35e2f72e4a05
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-07 22:59:17 +0100

    setup the bar chart so it works with calories eaten and the dates of the month

--- src/controllers/SummaryTabController.java
+++ src/controllers/SummaryTabController.java
@@ -2,12 +2,16 @@ package controllers;
 
 /* Import java, javafx, mainPackage */
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 
 public class SummaryTabController implements Initializable {
@@ -15,7 +19,17 @@ public class SummaryTabController implements Initializable {
 	@FXML
 	private PieChart dailyMacros;
 	
+	
+	@FXML
+	private BarChart<Number, Date> dailyProgress;
+	@FXML
+	private CategoryAxis categoryAxisDate;
+	@FXML
+	private NumberAxis numberAxisCalories;
+	
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		// Setup pie chart
 		ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList(
                 new PieChart.Data("Protein", 40),
@@ -25,5 +39,8 @@ public class SummaryTabController implements Initializable {
                 
 		dailyMacros.setData(pieChartData);
 		dailyMacros.setTitle("Daily Macros");
+		
+		// Setup bar chart
+		
 	}
 }
