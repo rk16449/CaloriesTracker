9c2268f4cb24678c5b17aab7d9f2899d726f328f
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-07 23:39:52 +0100

    added goals tab controller and connected it within its fxml page

---
+++ src/controllers/GoalsTabController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+/* Import java, javafx, mainPackage */
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
+
+public class GoalsTabController implements Initializable {
+
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+}
