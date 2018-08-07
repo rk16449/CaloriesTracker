567a395c3a1279b723d65210ad2ceb05bf6a639b
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-07 22:03:29 +0100

    add pie chart for daily macros summary (protein, carbs, fats)

--- src/controllers/SummaryTabController.java
+++ src/controllers/SummaryTabController.java
@@ -4,9 +4,14 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 
 public class SummaryTabController implements Initializable {
+	
+	@FXML
+	private PieChart dailyMacros;
 	
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
