793d12ba63330b7095daa364910b56aefb486067
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-11 08:58:17 +0100

    removed old user(now person) with new user class (to store email, id, date created, password/hash etc)

--- src/model/User.java
+++ src/model/User.java
@@ -1,55 +1,12 @@
 package model;
 
 public class User {
-
-	private static User instance = new User();
-
-	/*
-	 * Keep track of users: age, height, weight
-	 */
-	private int age;
-	private double weight;
-	private double height;
-
-	/* Singleton class */
-	private User() {
-
-	}
 	
-	//static block initialization for exception handling
-    static{
-        try{
-            instance = new User();
-        }catch(Exception e){
-            throw new RuntimeException("Exception occured in creating singleton user instance");
-        }
-    }
-
-	public static User getInstance() {
-		return instance;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public double getWeight() {
-		return weight;
-	}
-
-	public void setWeight(double weight) {
-		this.weight = weight;
-	}
-
-	public double getHeight() {
-		return height;
-	}
-
-	public void setHeight(double height) {
-		this.height = height;
-	}
+	/*
+	 * Class which stores temp account details
+	 */
+	
+	private int id;
+	private String email;
+	private String password;
 }
