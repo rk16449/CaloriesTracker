4b80f2566255ec949c3e4efce98fd7ff90384a7d
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-13 22:59:53 +0100

    fixed up some code, cleaned and removed dead code

--- src/controllers/AddFoodController.java
+++ src/controllers/AddFoodController.java
@@ -7,10 +7,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -19,12 +21,15 @@ import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import model.Food;
 
-
 public class AddFoodController implements Initializable {
-
+	
+	private Stage stage;
+	
 	/**********************************************************
 	 * TABLE GUI
 	 **********************************************************/
@@ -42,103 +47,102 @@ public class AddFoodController implements Initializable {
 	private TableColumn<Food, String> fatsColumn;
 	@FXML
 	private TableColumn<Food, String> proteinColumn;
-	
+
 	@FXML
 	private TextField textfieldSearch;
-	
+
 	@FXML
 	private Button buttonSearch, buttonAddFood;
-	
+
 	@FXML
 	private Spinner<Integer> spinnerQuantity;
+
+	
+	// The current selected food we need to pass back to the DietTabController
+	private Food returnFoodData;
+	
 	
 	// Hold the food data on the table in text form
 	private ObservableList<Food> foodData = FXCollections.observableArrayList();
 	// Hold the objects of foods
 	private ArrayList<Food> addedFoods = new ArrayList<Food>();
+
+	
 	
 	public void initialize(URL arg0, ResourceBundle arg1) {
+
 		// Fill tableview with dummy data (later from database)
-		
 		Food food1 = new Food("Whole Milk", 100, new double[] { 4.70, 3.70, 3.50 });
 		Food food2 = new Food("Protein Powder", 30, new double[] { 3.77, 0.2, 23.71 });
 		Food food3 = new Food("White Rice", 100, new double[] { 78.90, 0.70, 6.70 });
-		
+
 		// add to arraylist
 		addedFoods.add(food1);
 		addedFoods.add(food2);
 		addedFoods.add(food3);
-		
+
 		// Add sample data
-		for(int i=0; i<addedFoods.size(); i++) {
+		for (int i = 0; i < addedFoods.size(); i++) {
 			foodData.add(addedFoods.get(i));
 		}
-		
+
 		// Initialize the person table with the two columns.
 		foodColumn.setCellValueFactory(cellData -> cellData.getValue().getStrFood());
-		
+
 		amountColumn.setCellValueFactory(cellData -> cellData.getValue().getStrAmount());
 		caloriesColumn.setCellValueFactory(cellData -> cellData.getValue().getStrCalories());
-		
+
 		carbsColumn.setCellValueFactory(cellData -> cellData.getValue().getStrCarbs());
 		fatsColumn.setCellValueFactory(cellData -> cellData.getValue().getStrFats());
 		proteinColumn.setCellValueFactory(cellData -> cellData.getValue().getStrProts());
-		
-		
-		
+
 		FilteredList<Food> flFoods = new FilteredList<Food>(foodData, p -> true);
 
 		// Add filtered list data to the table
 		tableviewFoods.setItems(flFoods);
-		
-		
-		
-		
+
 		// Setup textfield filter (based off food name)
 		textfieldSearch.setPromptText("Search here!");
-        textfieldSearch.setOnKeyReleased(keyEvent ->
-        {
-        	System.out.println("textfield search activated");
-        	flFoods.setPredicate(p -> p.getName().toLowerCase().contains(textfieldSearch.getText().toLowerCase().trim()));
-        });
-        
-        
-        // Setup spinner to increase quantity >= 1
-        spinnerQuantity.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100));
-        
-        // TODO(minor bug) Typing over 100 will result in an error NumberFormatException 
-	}
-	
-	private Food returnFoodData;
-	
-	public Food getFood() {
-		return returnFoodData;
+		textfieldSearch.setOnKeyReleased(keyEvent -> {
+			System.out.println("textfield search activated");
+			flFoods.setPredicate(
+					p -> p.getName().toLowerCase().contains(textfieldSearch.getText().toLowerCase().trim()));
+		});
+
+		// Setup spinner to increase quantity >= 1
+		spinnerQuantity.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100));
+
+		// TODO(minor bug) Typing over 100 will result in an error NumberFormatException
+
 	}
 
-	// Listener to adding foods
 	@FXML
 	protected void handleAddFood(ActionEvent event) throws IOException {
 		System.out.println("Add the food to the table behind us!");
-		
 		try {
 			Food selectedFood = tableviewFoods.getSelectionModel().getSelectedItem();
 			System.out.println("We want to add: " + selectedFood.getName());
 			System.out.println("The quantity to add is: " + spinnerQuantity.getValue());
-			
-			// save the food selected to the return value this controller will return when closed
 			returnFoodData = selectedFood;
-			
-			// close the controller/window
-		    Stage stage = (Stage) buttonAddFood.getScene().getWindow();
-		    stage.close();
-			
-		}catch(NullPointerException e) {
+			stage.close();
+		} catch (NullPointerException e) {
 			System.out.println("A table row wasn't selected");
 		}
+	}
 
+	public String getData() {
+		return returnFoodData.getName();
 	}
 
 	public int getQuantity() {
 		return spinnerQuantity.getValue();
+	}
+
+	public void setStageAndSetupListeners(Stage stage) {
+		this.stage = stage;
+	}
+	
+	public Food getFood() {
+		return returnFoodData;
 	}
 }
--- src/controllers/DietTabController.java
+++ src/controllers/DietTabController.java
@@ -4,27 +4,21 @@ import java.io.IOException;
 /* Import java, javafx, mainPackage */
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import model.Food;
 
 public class DietTabController implements Initializable {
@@ -47,129 +41,92 @@ public class DietTabController implements Initializable {
 	private TableColumn<Food, String> fatsColumn;
 	@FXML
 	private TableColumn<Food, String> protsColumn;
-	
+
 	@FXML
 	private TableColumn<Food, String> quantityColumn;
-	
-	
+
 	@FXML
 	private PieChart pieChartMacros;
-	
-	@FXML 
+
+	@FXML
 	private Button addEntryButton;
 
 	// Hold the food data on the table in text form
 	private ObservableList<Food> foodData = FXCollections.observableArrayList();
 	// Hold the objects of foods
 	private ArrayList<Food> addedFoods = new ArrayList<Food>();
-	
+
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+
 		Food food1 = new Food("Whole Milk", 100, new double[] { 4.70, 3.70, 3.50 });
 		food1.setQuantity(3.6);
-		
+
 		Food food2 = new Food("Protein Powder", 30, new double[] { 3.77, 0.2, 23.71 });
 		food2.setQuantity(2);
-		
+
 		// add to arraylist
 		addedFoods.add(food1);
 		addedFoods.add(food2);
-		
+
 		double protein = 0, fats = 0, carbs = 0;
-		
+
 		// Add sample data
-		for(int i=0; i<addedFoods.size(); i++) {
-			
+		for (int i = 0; i < addedFoods.size(); i++) {
+
 			Food f = addedFoods.get(i);
-			
+
 			// Add up sum total of proteins, carbs, fats
 			protein += f.getProteins();
 			carbs += f.getCarbohydrates();
 			fats += f.getFats();
-			
+
 			foodData.add(f);
 		}
-		
 
 		// Initialize the person table with the two columns.
 		foodsColumn.setCellValueFactory(cellData -> cellData.getValue().getStrFood());
-		
 		amountColumn.setCellValueFactory(cellData -> cellData.getValue().getStrAmount());
 		caloriesColumn.setCellValueFactory(cellData -> cellData.getValue().getStrCalories());
-		
 		carbsColumn.setCellValueFactory(cellData -> cellData.getValue().getStrCarbs());
 		fatsColumn.setCellValueFactory(cellData -> cellData.getValue().getStrFats());
 		protsColumn.setCellValueFactory(cellData -> cellData.getValue().getStrProts());
-		
 		quantityColumn.setCellValueFactory(cellData -> cellData.getValue().getStrQuantity());
 
 		// Add observable list data to the table
 		tableviewEntries.setItems(foodData);
-		
-		
 
 		// Setup pie chart
-		ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                new PieChart.Data("Protein", protein),
-                new PieChart.Data("Fats", fats),
-                new PieChart.Data("Carbohydrates", carbs)
-                );
-                
+		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+				new PieChart.Data("Protein", protein), new PieChart.Data("Fats", fats),
+				new PieChart.Data("Carbohydrates", carbs));
+
 		pieChartMacros.setData(pieChartData);
 		pieChartMacros.setTitle("Daily Macros");
 	}
-	
+
 	@FXML
 	protected void handleAddEntry(ActionEvent event) throws IOException {
 		// Open a window which has a search bar to search for foods on the database
 		System.out.println("Create add food window here");
-		
+
 		try {
-	        FXMLLoader fxmlLoader = new FXMLLoader();
-	        fxmlLoader.setLocation(getClass().getResource("/view/dietTabAddFoodWindow.fxml"));
-	        /* 
-	         * if "fx:controller" is not set in fxml
-	         * fxmlLoader.setController(NewWindowController);
-	         */
-	        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-	        Stage stage = new Stage();
-	        
-	        Stage parent = (Stage) addEntryButton.getScene().getWindow();
-	        
-	        stage.initOwner(parent);                       
-	        stage.initModality(Modality.WINDOW_MODAL); 
-	        
-	        stage.setTitle("Add Entry");
-	        stage.setScene(scene);
-	        stage.show();
-	        
-	        AddFoodController controller = fxmlLoader.<AddFoodController>getController();
-	        
-	        stage.setOnCloseRequest((EventHandler<WindowEvent>) new EventHandler<WindowEvent>() {
-	            public void handle(WindowEvent we) {
-	            	
-	            	
-	            	
-	            	
-	            	int returnedQuantity = controller.getQuantity();
-	                Food returnedFood = controller.getFood();
-	                returnedFood.setQuantity(returnedQuantity);
-	                
-	                System.out.println(returnedFood.getName());
-	                
-	                // Add it to the arraylist & tableview items
-	                //addedFoods.add(returnedFood);
-	                //foodData.add(returnedFood);
-	            }
-	        });
-	        
-	        
-	    } catch (IOException e) {
-	        System.out.println("failed to create a window");
-	    }
-		
-		// There should also be a listbox to choose from the certain foods
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setLocation(getClass().getResource("/view/dietTabAddFoodWindow.fxml"));
+
+			Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+			Stage stage = new Stage();
+			Stage parent = (Stage) addEntryButton.getScene().getWindow();
+			stage.initOwner(parent);
+			stage.initModality(Modality.WINDOW_MODAL);
+			stage.setTitle("Add Entry");
+			stage.setScene(scene);
+			stage.show();
+			AddFoodController controller = fxmlLoader.<AddFoodController>getController();
+			controller.setStageAndSetupListeners(stage);
+
+		} catch (IOException e) {
+			System.out.println("Failed to create a window");
+		}
 	}
 
 }
