d44c808620f6eda1fa41c21e735f1b3eff5802fe
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-14 19:05:40 +0100

    EditFoodController.java (forgot to add before) but this handles the edit food button

---
+++ src/controllers/EditFoodController.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import java.io.IOException;
+/* Import java, javafx, mainPackage */
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import model.Food;
+
+public class EditFoodController implements Initializable {
+	
+	private Stage stage;
+	private double quantity = 1;
+	
+	@FXML 
+	private Button buttonSave;
+	
+	@FXML
+	private TextField textfieldQuantity;
+
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		textfieldQuantity.setText("1");
+	}
+	
+	@FXML
+	protected void handleSaveButton(ActionEvent event) throws IOException {
+		try {
+			quantity = Double.parseDouble(textfieldQuantity.getText());
+			buttonSave.getScene().getWindow().hide();
+		} catch (NullPointerException e) {
+			System.out.println("A table row wasn't selected");
+		}
+	}
+	
+	public void setTextFieldValue(String value) {
+		System.out.println("Set new value here!!!");
+		textfieldQuantity.setText(value);
+	}
+	
+	public double getQuantity() {
+		// Get quantity from textfield
+		quantity = Double.parseDouble(textfieldQuantity.getText());
+		return quantity;
+	}
+	
+	public void setStageAndSetupListeners(Stage stage) {
+		this.stage = stage;
+	}
+}
