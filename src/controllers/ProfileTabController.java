fdd62472a1495fdc0c2ea664e14aed93d4be15b2
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-17 18:26:53 +0100

    added button event handlers to profile tab

--- src/controllers/ProfileTabController.java
+++ src/controllers/ProfileTabController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.io.IOException;
 /* Import java, javafx, mainPackage */
 import java.net.URL;
 import java.util.Date;
@@ -7,6 +8,7 @@ import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
@@ -14,10 +16,20 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 public class ProfileTabController implements Initializable {
 	
-	@FXML Button buttonEditProfile;
+	@FXML
+	TextField tfFirstName, tfLastName, tfAge, tfHeight, tfWeight, tfBodyfat, tfWaist;
+	
+	@FXML
+	ChoiceBox cbGender;
+
+	@FXML 
+	Button btnEditProfile, btnMetric, btnImperial;
 	
 
 	// PROFILE TAB
@@ -39,6 +51,22 @@ public class ProfileTabController implements Initializable {
 	// All this data will then be updated on the database and reflect on the calculators
 	
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+	
+	
+	@FXML
+	protected void handleEditProfile(ActionEvent event) throws IOException {
+		
+	}
+	
+	@FXML
+	protected void handleMetric(ActionEvent event) throws IOException {
+		
+	}
+	
+	@FXML
+	protected void handleImperial(ActionEvent event) throws IOException {
 		
 	}
 }
