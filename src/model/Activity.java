eee38b1536ff98098056a8d88e7781595169f69e
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-18 21:18:57 +0100

    activity now extends item, also included activityLevel and getters/setters

--- src/model/Activity.java
+++ src/model/Activity.java
@@ -1,11 +1,20 @@
 package model;
 
-public class Activity {
+public class Activity extends Item {
 	// Used to calculate TDEE based off activity levels
 	
 	private double activityLevel;
 	
-	public Activity() {
-		
+	public Activity(String name, double level) {
+		super(name);
+		this.setActivityLevel(level);
+	}
+
+	public double getActivityLevel() {
+		return activityLevel;
+	}
+
+	public void setActivityLevel(double activityLevel) {
+		this.activityLevel = activityLevel;
 	}
 }
