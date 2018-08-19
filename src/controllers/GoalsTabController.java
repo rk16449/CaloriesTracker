2b45b2f18eee431fd45bc9f6dee86c2b8ec4c3f8
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-19 20:41:07 +0100

    added currentGoal and assigned method to each button to change the goal, textfield will also update with correct goal set

--- src/controllers/GoalsTabController.java
+++ src/controllers/GoalsTabController.java
@@ -1,20 +1,15 @@
 package controllers;
 
+import java.io.IOException;
 /* Import java, javafx, mainPackage */
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import model.Goal;
@@ -25,9 +20,8 @@ public class GoalsTabController implements Initializable {
 	
 	@FXML Button btnMaintainWeight, btnGainWeight, btnLoseWeight;
 	
-	
+	private Goal currentGoal;
 	private ArrayList<Goal> goals = new ArrayList<Goal>();
-	
 	
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
@@ -37,5 +31,34 @@ public class GoalsTabController implements Initializable {
 		Goal gainWeight = new Goal("Gain Weight", 1.2);
 		
 		goals.addAll(Arrays.asList(loseWeight, maintainWeight, gainWeight));
+	}
+	
+	private Goal getGoal(String name) {
+		for(int i=0; i<goals.size(); i++) {
+			if(goals.get(i).getName().equals(name)) {
+				return goals.get(i);
+			}
+		}
+		return null;
+	}
+	
+	private void updateGoal(String name) {
+		currentGoal = getGoal(name);
+		tfCurrentGoal.setText(currentGoal.getName());
+	}
+	
+	@FXML
+	protected void handleLoseWeight(ActionEvent event) throws IOException {
+		updateGoal("Lose Weight");
+	}
+	
+	@FXML
+	protected void handleGainWeight(ActionEvent event) throws IOException {
+		updateGoal("Gain Weight");
+	}
+	
+	@FXML
+	protected void handleMaintainWeight(ActionEvent event) throws IOException {
+		updateGoal("Maintain Weight");
 	}
 }
