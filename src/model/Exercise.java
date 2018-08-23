1feb6a497b412a0f61176bfba69335ae675a91d5
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-23 21:31:41 +0100

    added reps, sets, weight, caloriesBurned attributes and getters/setters to Exercise.java

--- src/model/Exercise.java
+++ src/model/Exercise.java
@@ -5,13 +5,52 @@ import javafx.beans.property.StringProperty;
 
 public class Exercise extends Item {
 	
+	private int reps;
+	private int sets;
+	private double weight;
+	private double caloriesBurned;
+	
 	public Exercise(String name) {
 		super(name);
 	}
+	
+	// TableView Getters
 
 	public StringProperty getStrExercise() {
 		return new SimpleStringProperty(this.getName());
 	}
 	
+	// Getters & Setters
 	
+	public int getReps() {
+		return reps;
+	}
+
+	public void setReps(int reps) {
+		this.reps = reps;
+	}
+
+	public int getSets() {
+		return sets;
+	}
+
+	public void setSets(int sets) {
+		this.sets = sets;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getCaloriesBurned() {
+		return caloriesBurned;
+	}
+
+	public void setCaloriesBurned(double caloriesBurned) {
+		this.caloriesBurned = caloriesBurned;
+	}
 }
