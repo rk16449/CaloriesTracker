07f7e1a7b8425007bc4a322eb3c03f3cfa69a880
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-24 17:38:20 +0100

    added dummy exercises data, linked data with the tables

--- src/controllers/AddExerciseController.java
+++ src/controllers/AddExerciseController.java
@@ -1,15 +1,79 @@
 package controllers;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import model.Exercise;
+import model.Item;
 
 public class AddExerciseController implements Initializable {
+	
+	@FXML
+	TableView<Exercise> tvExercises;
+	
+	@FXML
+	TableColumn<Exercise, String> tcExercise;
+	
+	@FXML
+	Button btnAddExercise, btnSearch;
+	
+	@FXML
+	TextField tfSearch, tfSets, tfReps, tfWeight;
 
+	// The current selected Exercise we need to pass back to the DietTabController
+	private Exercise returnExerciseData;
+	// Hold the Exercise data on the table in text form
+	public static ObservableList<Exercise> exerciseData = FXCollections.observableArrayList();
+	// Hold the objects of Exercises
+	public static ArrayList<Exercise> addedExercises = new ArrayList<Exercise>();
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
+		// Create Dummy Data for table
+		if(addedExercises.size() == 0) {
+			Exercise e1 = new Exercise("Bench Press");
+			Exercise e2 = new Exercise("Squat");
+			Exercise e3 = new Exercise("Deadlift");
+			Exercise e4 = new Exercise("Overhead Press");
+			Exercise e5 = new Exercise("Barbell Row");
+			
+			addedExercises.addAll(Arrays.asList(e1, e2, e3, e4, e5));
+			exerciseData.addAll(Arrays.asList(e1, e2, e3, e4, e5));
+		}
+		
+		FilteredList<Exercise> flExercises = new FilteredList<Exercise>(exerciseData, p -> true);
+
+		
+		// Setup column values
+		tcExercise.setCellValueFactory(e -> e.getValue().getStrExercise());
+		
+		// Add filtered list data to the table
+		tvExercises.setItems(flExercises);
+		
+		
+		// Setup textfield filter (based off food name)
+		tfSearch.setPromptText("Search here!");
+		tfSearch.setOnKeyReleased(keyEvent -> {
+			System.out.println("textfield search activated");
+			flExercises.setPredicate(
+					p -> p.getName().toLowerCase().contains(tfSearch.getText().toLowerCase().trim()));
+		});
+	}
+
+	public Item getExercise() {
+		return returnExerciseData;
 	}
 
 }
--- src/controllers/ExercisesTabController.java
+++ src/controllers/ExercisesTabController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 /* Import java, javafx, mainPackage */
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.ResourceBundle;
 
@@ -11,7 +12,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
@@ -20,6 +23,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.Exercise;
 import model.Food;
 
@@ -38,6 +43,8 @@ public class ExercisesTabController implements Initializable {
 	
 	@FXML
 	Button btnAddExercise, btnCustom, btnEdit, btnDelete;
+	
+
 	
 	
 	// Start of ExercisesTabController runs on creation
@@ -67,7 +74,79 @@ public class ExercisesTabController implements Initializable {
 		
 		System.out.println("Open add Exercise window");
 		
+		try {
+
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setLocation(getClass().getResource("/view/exercisesTabAddExerciseWindow.fxml"));
+			Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+			Stage stage = new Stage();
+			Stage parent = (Stage) btnAddExercise.getScene().getWindow();
+			stage.initOwner(parent);
+			stage.initModality(Modality.WINDOW_MODAL);
+			stage.setTitle("Add Entry");
+			stage.setScene(scene);
+
+			// Setup controller
+			AddExerciseController controller = fxmlLoader.<AddExerciseController>getController();
+
+			// showAndWait will block execution until the window closes...
+			stage.showAndWait();
+
+			// continue on adding entry with controller
+			addEntry(controller);
+
+		} catch (IOException e) {
+			System.out.println("Failed to create a window");
+		}
+		
+		
 	}
+	
+	
+	private void addEntry(AddExerciseController controller) {
+		try {
+			System.out.println("ExercieTabController: " + controller.getExercise().getName());
+			boolean found = false;
+			// Check if this food already exists in the table, if it does increase its
+			// quantity instead
+			
+			/*
+			for (int i = 0; i < currentDay.getFoods().size(); i++) {
+				// Assumes we don't have foods with exactly the same name.. (try adding id in
+				// later)
+				if (currentDay.getFoods().get(i).getName().equals(controller.getFood().getName())) {
+					found = true;
+					
+					System.out.println("Updating quantity on addEntry");
+					
+					currentDay.getFoods().get(i).setQuantity(currentDay.getFoods().get(i).getQuantity() + controller.getQuantity());
+					break;
+				}
+			}
+
+			// Add a new row entry if same food isn't already added
+			if (!found) {
+				System.out.println("No Food was found, creating a new entry here!");
+				
+				// Copy the object
+				Food newFood = new Food(controller.getFood().getName(), controller.getFood());
+				newFood.setQuantity(controller.getQuantity()); // maybe do this automatically on getFood()
+
+				// Add values to the table!
+				addedFoods.add(newFood);
+				foodData.add(newFood);
+				currentDay.addFood(newFood);
+			}
+
+			// Update GUI
+			update();
+			*/
+		} catch (NullPointerException e) {
+			System.out.println("Nullpointerexception, probably because we hit the X/ no exercise selected");
+		}
+	}
+	
+	
 	
 	@FXML
 	protected void handleDelete(ActionEvent event) throws IOException {
