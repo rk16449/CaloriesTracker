f8523db17b1d644231c744eab98763efbc17a107
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-24 23:58:51 +0100

    added EditExerciseController.java and linked in fxml

---
+++ src/controllers/EditExerciseController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class EditExerciseController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+
+}
