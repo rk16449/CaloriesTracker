b378bbd869a83e7622cc68350425c75bacd90cb2
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-25 00:09:27 +0100

    added references to fx:ids in edit exercise window

--- src/controllers/EditExerciseController.java
+++ src/controllers/EditExerciseController.java
@@ -1,15 +1,30 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class EditExerciseController implements Initializable {
 
+	@FXML
+	TextField tfSets, tfReps, tfWeight, tfCaloriesBurned;
+	
+	@FXML
+	Button btnSave;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 	}
+	
+	@FXML
+	protected void handleSave(ActionEvent event) throws IOException {
 
+	}
 }
