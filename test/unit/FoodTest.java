fae0711340b13f34c9b1208c909b9196b1de127e
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-28 19:28:23 +0100

    Starting on JUnit testing; testing constructors of Food

--- test/unit/FoodTest.java
+++ test/unit/FoodTest.java
@@ -5,7 +5,11 @@ package unit;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import model.Food;
 
 /**
  * @author Raj
@@ -13,9 +17,14 @@ import org.junit.Test;
  */
 public class FoodTest {
 
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void throwsIllegalArgumentExceptionIfArrayEmpty() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Invalid array size");
+		Food f1 = new Food("Cake", new double[] {}, false);
 	}
 
 }
