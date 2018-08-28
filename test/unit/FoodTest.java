e1a49572b3f8af83cc5d5692e0e68eb8ae4af96a
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-28 16:49:43 +0100

    added FoodTest.java

---
+++ test/unit/FoodTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Raj
+ *
+ */
+public class FoodTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
