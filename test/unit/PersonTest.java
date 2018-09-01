e56fb19b8f6eb959e2d50cd20cf4f227ff7b0285
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-01 23:18:11 +0100

    added PersonTest.java

---
+++ test/unit/PersonTest.java
@@ -0,0 +1,19 @@
+package unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PersonTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
