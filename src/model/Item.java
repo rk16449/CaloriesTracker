e8129626e66713023361f151551bc16d5f95086f
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-02 22:43:58 +0100

    refactor; Item.java moved constructor code to separate method

--- src/model/Item.java
+++ src/model/Item.java
@@ -9,7 +9,13 @@ public abstract class Item {
 	
 	public Item(String name) {
 		this.name = name;
-		
+		validItem();
+	}
+	
+	/**
+	 * Decides if this item can be valid, i.e we haven't reached the maximum amount of items before we can set ID
+	 */
+	private void validItem() {
 		if(start_id >= Integer.MAX_VALUE) throw new IllegalArgumentException("Maximum items reached!");
 		
 		this.id = start_id;
